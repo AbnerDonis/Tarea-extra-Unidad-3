d53160811423a9c97f5e0adc706b77e33ffcc2f7
AbnerDonis <18001641@galileo.edu>
2025-02-15 11:10:32 -0600

    Add files via upload

---
+++ Unidad 3/signosDelZodiaco.js
@@ -0,0 +1,34 @@
+ 
+var dia = prompt("ingresa en uno o dos digitos el dia de tu nacimiento:"); // Pide el día
+var mes = prompt("Ingresa el mes de tu nacimiento (número):"); // Pide el mes
+
+dia = parseInt(dia);
+mes = parseInt(mes);
+
+if ((mes == 1 && dia >= 20) || (mes == 2 && dia <= 18)) {
+  alert("eres Acuario");
+} else if ((mes == 2 && dia >= 19) || (mes == 3 && dia <= 20)) {
+  alert("eres Piscis");
+} else if ((mes == 3 && dia >= 21) || (mes == 4 && dia <= 19)) {
+  alert("eres Aries");
+} else if ((mes == 4 && dia >= 20) || (mes == 5 && dia <= 20)) {
+  alert("eres Tauro");
+} else if ((mes == 5 && dia >= 21) || (mes == 6 && dia <= 20)) {
+  alert("eres geminis");
+} else if ((mes == 6 && dia >= 21) || (mes == 7 && dia <= 22)) {
+  alert("eres Cáncer");
+} else if ((mes == 7 && dia >= 23) || (mes == 8 && dia <= 22)) {
+  alert("eres Leo");
+} else if ((mes == 8 && dia >= 23) || (mes == 9 && dia <= 22)) {
+  alert("eres Virgo");
+} else if ((mes == 9 && dia >= 23) || (mes == 10 && dia <= 22)) {
+  alert("eres Libra");
+} else if ((mes == 10 && dia >= 23) || (mes == 11 && dia <= 21)) {
+  alert("eres Escorpio");
+} else if ((mes == 11 && dia >= 22) || (mes == 12 && dia <= 21)) {
+  alert("eres Sagitario");
+} else if ((mes == 12 && dia >= 22) || (mes == 1 && dia <= 19)) {
+  alert("eres capricornio");
+} else {
+  alert("Fecha inválida");
+}
